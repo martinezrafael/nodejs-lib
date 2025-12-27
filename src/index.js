cab73ce1bc2fdcaba25bfc396427e931ff942c15
martinezrafael <rafaelmartinez.contato@gmail.com>
2025-12-26 23:57:52 -0300

    Adiciona um readme `iniciando-um-projeto-com-node.md`

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ const path = process.argv;
 const link = path[2];
 
 fs.readFile(link, "utf-8", (err, text) => {
-  verificaPalavrasDuplicadas(text);
+  quebraEmParagrafos(text);
+  // verificaPalavrasDuplicadas(text);
 });
+
+function quebraEmParagrafos(text) {
+  const paragrafos = text.toLowerCase().split("\n");
+  const contagem = paragrafos.map((paragrafo) => {
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
+  console.log(contagem);
+}
 
 // criar um array com as palavras
 // contar as ocorrencias
@@ -25,5 +34,5 @@ function verificaPalavrasDuplicadas(text) {
     resultado[palavra] = (resultado[palavra] || 0) + 1;
   });
 
-  console.log(resultado);
+  return resultado;
 }
