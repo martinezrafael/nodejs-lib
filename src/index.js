b023c94088ed6f55473c1ec9a232e09398b66694
martinezrafael <rafaelmartinez.contato@gmail.com>
2025-12-31 13:18:49 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-
 // fs = file system
+const fs = require("fs");
+const { extname } = require("path");
 
 const path = process.argv;
 const link = path[2];
 
 fs.readFile(link, "utf-8", (err, text) => {
+  if (err) throw err;
   quebraEmParagrafos(text);
-  // verificaPalavrasDuplicadas(text);
 });
 
 function quebraEmParagrafos(text) {
@@ -15,20 +15,22 @@ function quebraEmParagrafos(text) {
   const contagem = paragrafos.map((paragrafo) => {
     return verificaPalavrasDuplicadas(paragrafo);
   });
-  console.log(contagem);
 }
 
-// criar um array com as palavras
-// contar as ocorrencias
-// montar um objeto com o resultado
-/*{
-  "web": 5,
-  "computador": 4
-}*/
+/* 
+  - criar um array com as palavras
+  - contar as ocorrencias
+  - montar um objeto com o resultado
+  {
+    "web": 5,
+    "computador": 4,
+    ...
+  }
+*/
+
 function verificaPalavrasDuplicadas(text) {
   const listaPalavras = text.split(" ");
   const resultado = {};
-  // objeto[propriedade] = valor;
 
   listaPalavras.forEach((palavra) => {
     resultado[palavra] = (resultado[palavra] || 0) + 1;
@@ -36,3 +38,13 @@ function verificaPalavrasDuplicadas(text) {
 
   return resultado;
 }
+
+// Exemplo prático de uso
+const frase = "JavaScript é muito poderoso";
+
+const palavras = frase.split(" ");
+
+console.log(palavras);
+
+// Resultado
+["JavaScript", "é", "muito", "poderoso"];
